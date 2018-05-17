00dd4b1fde6a304b7e0437d5243247a90879d6af
Thiago Sales <thiagomsales@edu.unifor.br>
2018-05-17 10:58:28 -0300

    0.1

---
+++ redes.ino
@@ -0,0 +1,45 @@
+void setup() {
+  Serial.begin(9600)
+
+}
+
+void enviarEstadoLivre(){
+  enviarMensagem("Livre");
+}
+void enviarEstadoTrabalhando(){
+  enviarMensagem("Trabalhando");
+}
+void enviarEstadoTerminado(){
+  enviarMensagem("Terminado");
+}
+void enviarMensagem(String mensagem){
+  serial.write(mensagem);
+}
+String esperarMensagemDoMestre(){//string
+  if (Serial.available() > 0) { 
+    String str = Serial.readString(); 
+    //teste
+    return str;
+  }
+}
+
+void pararGarrafa(){
+  enviarEstadoTrabalhando();
+}
+void tamparGarrafa(){
+  
+  //ações
+  enviarEstadoTerminado();
+}
+void liberarGarrafa(){
+  esperarMensagemDoMestre();
+  //libera garrafa
+
+  enviarEstadoLivre();
+}
+
+void loop() {
+  // if(esperarMensagemDoMestre()== "Começar"){pararGarrafa()tamparGarrafa()liberarGarrafa()}
+
+
+}
