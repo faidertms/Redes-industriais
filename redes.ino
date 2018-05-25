3d29697510b111f7441566ae0392ac81e8ee167c
Thiago Sales <thiagomsales@edu.unifor.br>
2018-05-25 01:07:09 -0300

    Update redes.ino

--- redes.ino
+++ redes.ino
@@ -3,35 +3,41 @@
 
 unsigned long tempo;
 char alfa[] = "abcdefghijklmnopqrstuvwxyz";
-char cripts[][27] = {{'8','F','H','G','3','l','1','L','E','I','w','o','M','X','6','Q','P','b','V','9','a','Z','S','D','j','r'},{'z','-','k','m','x','n','B','u','0','-','-','O','A','v','5','p','-','R','y','f','4','g','-','-','-','-'},{'K','-','s','N','q','-','-','-','J','-','-','a','c','-','2','-','-','T','e','Y','h','-','-','-','-','-'},{'7','-','-','-','t','-','-','-','-','-','-','-','-','-','W','-','-','C','-','-','-','-','-','-','-','-'},{'i','-','-','-','-','-','-','-','-','-','-','-','-','-','d','-','-','-','-','-','-','-','-','-','-','-','-'}};  
+char cripts[][27] = {{'8','F','H','G','3','l','1','L','E','I','w','o','M','X','6','Q','P','b','V','9','a','Z','S','D','j','r'},
+                    {'z','-','k','m','x','n','B','u','0','-','-','O','A','v','5','p','-','R','y','f','4','g','-','-','-','-'},
+                    {'K','-','s','N','q','-','-','-','J','-','-','-','c','-','2','-','-','T','e','Y','h','-','-','-','-','-'},
+                    {'7','-','-','-','t','-','-','-','-','-','-','-','-','-','W','-','-','C','-','-','-','-','-','-','-','-'},
+                    {'i','-','-','-','-','-','-','-','-','-','-','-','-','-','d','-','-','-','-','-','-','-','-','-','-','-'}};  
 String mensagemNova;
 bool recebeu = false;
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  enviarEstadoChegou();
  }
-
 //-------------------------------MENSAGENS-SERIAL--------------------------------------------\\
 
 void enviarEstadoLivre(){
-  enviarMensagem("Livre");
+  enviarMensagem("livre");
 }
 void enviarEstadoTrabalhando(){
-  enviarMensagem("Trabalhando");
+  enviarMensagem("trabalhando");
 }
 void enviarEstadoTerminado(){
-  enviarMensagem("Terminado");
+  enviarMensagem("terminou");
 }
 void enviarEstadoChegou(){
-  enviarMensagem("Chegou");
+  enviarMensagem("chegou");
 }
 void enviarMensagem(String mensagem){
   Serial.write(cripto(mensagem).c_str());
 }
 void esperarMensagemDoMestre(){//string
-  if(Serial.available()>0) {  
-    mensagemNova += descripto(String((char)Serial.read()));
-    Serial.println(mensagemNova);
+  if(Serial.available()) {  
+    mensagemNova += descripto((char)Serial.read());
     recebeu = true;
     tempo = millis();
   }
@@ -41,25 +47,6 @@ void limparMensagem(){
     recebeu = false;
 }
 
-//-------------------------------PROCESSOS--------------------------------------------\\
-
-void pararGarrafa(){
-  //procura a garrafa com algum sensor, se chegar..
-  if("chego" == "chego"){ // temporario
-    enviarEstadoTrabalhando();
-  }else{
-    enviarEstadoLivre();
-  }
-  
-}
-void tamparGarrafa(){
-  //ações
-  enviarEstadoTerminado();
-}
-void liberarGarrafa(){
-  //libera garrafa
-  enviarEstadoLivre();
-}
 //-------------------------------CRIPTOGRAFIA--------------------------------------------\\
 
 String descripto(String mensagemCripto ){
@@ -71,6 +58,25 @@ String descripto(String mensagemCripto ){
       for (int y= 0 ; y< 26; y++){
         for (int z = 0; z < 5; z++){
           if(strcmp(String(mensagemCripto[x]).c_str(),String(cripts[z][y]).c_str()) == 0){
+            mensagem += alfa[y];
+            goto achou;
+          }
+        }
+      }
+    }achou:;
+  }
+  return mensagem;
+}
+
+String descripto(char mensagemCripto ){
+  String mensagem;
+  for (int x = 0; x < 1; x++){
+    if(mensagemCripto == ' '){
+      mensagem += ' '; 
+    }else{
+      for (int y= 0 ; y< 26; y++){
+        for (int z = 0; z < 5; z++){
+          if(strcmp(&mensagemCripto,String(cripts[z][y]).c_str()) == 0){
             mensagem += alfa[y];
             goto achou;
           }
@@ -99,7 +105,27 @@ String cripto(String mensagem){
   
   return mensagemCripto;
 }
+//-------------------------------PROCESSOS--------------------------------------------\\
 
+void pararGarrafa(){
+  //procura a garrafa com algum sensor, se chegar..
+  //if("chego" == "chego"){ // temporario
+    enviarEstadoTrabalhando();
+  /*}else{
+    enviarEstadoLivre();
+  }*/
+  
+}
+void tamparGarrafa(){
+  //ações
+  delay(5000);
+  enviarEstadoTerminado();
+}
+void liberarGarrafa(){
+  //libera garrafa
+  delay(5000);
+  enviarEstadoLivre();
+}
 //-------------------------------LOOP-PRINCIPAL--------------------------------------------\\
 
 void loop() {
@@ -108,11 +134,11 @@ void loop() {
     limparMensagem();
     pararGarrafa();
     tamparGarrafa();
-    enviarMensagem("teste");
+   // enviarMensagem("teste");
   }else if(strcmp(mensagemNova.c_str(),"liberar") == 0){ // processo 2
     limparMensagem();
     liberarGarrafa();
-    enviarMensagem("thiago");
+    //enviarMensagem("thiago");
   }
   if(recebeu & ((millis()-tempo) == 1000)){ // Limpeza caso mensagem venha errado
     limparMensagem(); 
